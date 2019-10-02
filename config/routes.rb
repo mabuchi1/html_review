18227b64dcee9b678a84340c531e44df4bb721bc
mabuchi1 <msanhm6@gmail.com>
2019-10-02 19:49:12 +0900

    marge

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+=======
+  root to: "portfolio#index"
+  resources :profile, only: [:index]
+>>>>>>> Stashed changes
 end
