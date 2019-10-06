8251f7a929059057aff80d72f390b5104b157435
mabuchi1 <msanhm6@gmail.com>
2019-10-06 22:44:43 +0900

    blog_edit_destroy

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -9,12 +9,27 @@ class BlogsController < ApplicationController
     
     def create
     Blog.create(blog_params)
-    binding.pry
     redirect_to blogs_path
     end
 
     def show
       @blog = Blog.find(params[:id])
+    end
+
+    def edit
+      @blog = Blog.find(params[:id])
+    end
+
+    def update
+      blog = Blog.find(params[:id])
+      blog.update(blog_params)
+      redirect_to blog_path(blog.id)
+    end
+
+    def destroy
+      blog = Blog.find(params[:id])
+      blog.destroy
+      redirect_to blogs_path
     end
     
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "portfolio#index"
   resources :profile, only: [:index]
-  resources :blogs, only: [:new, :create, :show]
+  resources :blogs, only: [:new, :create, :show, :edit, :update, :destroy]
   get "/blogs", to:  "blogs#top"
 end
