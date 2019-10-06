50b256f7ae539da4f129c33321563d2a51c3fa58
mabuchi1 <msanhm6@gmail.com>
2019-10-06 19:27:02 +0900

    blog_show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "portfolio#index"
   resources :profile, only: [:index]
-  resources :blogs, only: [:new, :create]
+  resources :blogs, only: [:new, :create, :show]
   get "/blogs", to:  "blogs#top"
 end
