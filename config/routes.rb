22aff23276aeb69208bab70b825b68390aaaf9d2
mabuchi1 <msanhm6@gmail.com>
2019-10-08 06:51:32 +0900

    search

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :profile, only: [:index]
   resources :blogs, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: :create
+    collection do
+      get 'search'
+    end
   end
   get "/blogs", to:  "blogs#top"
   
